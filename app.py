8dbf91d836344ad42771f9eaaa13b65f23e8bd29
mateussfortes <mateussilveirafortes@gmail.com>
2024-07-15 18:48:26 -0300

    Manipulando strings

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print('Hello World')
+print('Sabor express')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção: {opcao_escolhida}')
+
+
+nome = input('What is your name: ')
+idade = input('What is your age: ')
+
+print(f'Meu nome é {nome} e minha idade {idade}')
+
+print('A\nL\nU\nR\nA\n')
+
